3163dc0795bafbe2adac2197dd987020a52e3486
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-02-17 11:02:25 +0530

    bug fix

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -10,12 +10,12 @@ function Navbarkids() {
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
         {/* <Nav.Link href="/"><img src="https://img.icons8.com/ultraviolet/26/000000/home--v1.png"/></Nav.Link> */}
-        <Nav.Link href="/alphabets"><img src="https://img.icons8.com/color/26/000000/alpha.png" alt="Alphabets_icon"/> Alphabets </Nav.Link>
-        <Nav.Link href="/numbers"><img src="https://img.icons8.com/color/26/000000/1-circle--v3.png" alt="number_icon"/> Numbers </Nav.Link>
-        <Nav.Link href="/words"><img src="https://img.icons8.com/color/26/000000/document--v1.png" alt="words_icon"/> Words </Nav.Link>
-        <Nav.Link href="/table"><img src="https://img.icons8.com/color/26/000000/sigma.png" alt="table_icon"/> Table's </Nav.Link>
+        <Nav.Link href="/kids-learning/#/alphabets"><img src="https://img.icons8.com/color/26/000000/alpha.png" alt="Alphabets_icon"/> Alphabets </Nav.Link>
+        <Nav.Link href="/kids-learning/#/numbers"><img src="https://img.icons8.com/color/26/000000/1-circle--v3.png" alt="number_icon"/> Numbers </Nav.Link>
+        <Nav.Link href="/kids-learning/#/words"><img src="https://img.icons8.com/color/26/000000/document--v1.png" alt="words_icon"/> Words </Nav.Link>
+        <Nav.Link href="/kids-learning/#/table"><img src="https://img.icons8.com/color/26/000000/sigma.png" alt="table_icon"/> Table's </Nav.Link>
         {/* <Nav.Link >Practice<img src="https://img.icons8.com/color/26/000000/edit--v1.png"/></Nav.Link> */}
-        <Nav.Link href="/about"><img src="https://img.icons8.com/color/26/000000/about.png" alt="About_icon"/> About </Nav.Link>
+        <Nav.Link href="/kids-learning/#/about"><img src="https://img.icons8.com/color/26/000000/about.png" alt="About_icon"/> About </Nav.Link>
         {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">  
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -25,7 +25,7 @@ function Navbarkids() {
         </NavDropdown> */}
       </Nav>
       <Form inline>
-          <Button href="/practice" variant="outline-success"> Practice <img src="https://img.icons8.com/color/26/000000/edit--v1.png" alt="Practice_icon"/></Button>
+          <Button href="/kids-learning/#/practice" variant="outline-success"> Practice <img src="https://img.icons8.com/color/26/000000/edit--v1.png" alt="Practice_icon"/></Button>
       </Form>
     </Navbar.Collapse>
   </Navbar>
