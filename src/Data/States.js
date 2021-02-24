40160f958a9d520a75586cf4f65c1fc20d22f379
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-02-24 18:50:36 +0530

    states added

---
+++ src/Data/States.js
@@ -0,0 +1,187 @@
+import {Row,Col,Image,Card} from 'react-bootstrap';
+
+import Amaravati from './StatesImg/Amaravati.jpg';
+import Itanagar from './StatesImg/Itanagar.jpg';
+import Dispur from './StatesImg/Dispur.jpg';
+import Patna from './StatesImg/Patna.jpg';
+import Rajpur from './StatesImg/Rajpur.jpg';
+import Panaji from './StatesImg/Panaji.jpg';
+import Gandhinagar from './StatesImg/Gandhinagar.jpg';
+import Chandigarh from './StatesImg/Chandigarh.jpg';
+import Shimla from './StatesImg/Shimla.jpg';
+import Ranchi from './StatesImg/Ranchi.jpg';
+import Bangalore from './StatesImg/Bangalore.jpg';
+import Thiruvananthapuram from './StatesImg/Thiruvananthapuram.jpg';
+import Bhopal from './StatesImg/Bhopal.jpg';
+import Mumbai from './StatesImg/Mumbai.jpg';
+import Imphal from './StatesImg/Imphal.jpg';
+import Shillong from './StatesImg/Shillong.jpg';
+import Aizawl from './StatesImg/Aizawl.jpg';
+import Kohima from './StatesImg/Kohima.jpg';
+import Bhubaneshwar from './StatesImg/Bhubaneshwar.jpg';
+import Jaipur from './StatesImg/Jaipur.jpg';
+import Gangtok from './StatesImg/Gangtok.jpg';
+import Chennai from './StatesImg/Chennai.jpg';
+import Hyderabad from './StatesImg/Hyderabad.jpg';
+import Agartala from './StatesImg/Agartala.jpg';
+import Dehradun from './StatesImg/Dehradun.jpg';
+import Lucknow from './StatesImg/Lucknow.jpg';
+import Kolkata from './StatesImg/Kolkata.jpg';
+
+import './States.css';
+
+function States() {
+   
+   const Data = [
+    {
+        photo:Amaravati,
+        state:"Andhra Pradesh",
+        capital:"Amaravati"
+    },
+    {
+        photo:Itanagar,
+        state:"Arunachal Pradesh",
+        capital:"Itanagar"
+    },
+    {
+        photo:Dispur,
+        state:"Assam",
+        capital:"Dispur"
+    },
+    {
+        photo:Patna,
+        state:"Bihar",
+        capital:"Patna"
+    },
+    {
+        photo:Rajpur,
+        state:"Chhattisgarh",
+        capital:"Rajpur"},
+    {
+        photo:Panaji,
+        state:"Goa",
+        capital:"Panaji"},
+    {
+        photo:Gandhinagar,
+        state:"Gujarat",
+        capital:"Gandhinagar"},
+    {
+        photo:Chandigarh,
+        state:"Haryana",
+        capital:"Chandigarh"
+    },
+    {
+        photo:Shimla,
+        state:"Himachal Pradesh",
+        capital:"Shimla"},
+    {
+        photo:Ranchi,
+        state:"Jharkhand",
+        capital:"Ranchi"},
+    {
+        photo:Bangalore,
+        state:"Karnataka",
+        capital:"Bangalore"},
+    {
+        photo:Thiruvananthapuram,
+        state:"Kerala",
+        capital:"Thiruvananthapuram"},
+    {
+        photo:Bhopal,
+        state:"Madhya Pradesh",
+        capital:"Bhopal"},
+    {
+        photo:Mumbai,
+        state:"Maharashtra",
+        capital:"Mumbai"},
+    {
+        photo:Imphal,
+        state:"Manipur",
+        capital:"Imphal"},
+    {
+        photo:Shillong,
+        state:"Meghalaya",
+        capital:"Shillong"},
+    {
+        photo:Aizawl,
+        state:"Mizoram",
+        capital:"Aizawl"},
+    {
+        photo:Kohima,
+        state:"Nagaland",
+        capital:"Kohima"},
+    {
+        photo:Bhubaneshwar,
+        state:"Odisha",
+        capital:"Bhubaneshwar"},
+    {
+        photo:Chandigarh,
+        state:"Punjab",
+        capital:"Chandigarh"},
+    {
+        photo:Jaipur,
+        state:"Rajasthan",
+        capital:"Jaipur"},
+    {
+        photo:Gangtok,
+        state:"Sikkim",
+        capital:"Gangtok"
+    },
+    {
+        photo:Chennai,
+        state:"Tamil Nadu",
+        capital:"Chennai"
+    },
+    {
+        photo:Hyderabad,
+        state:"Telangana",
+        capital:"Hyderabad"
+    },
+    {
+        photo:Agartala,
+        state:"Tripura",
+        capital:"Agartala"
+    },
+    {
+        photo:Dehradun,
+        state:"Uttarakhand",
+        capital:"Dehradun"
+    },
+    {
+        photo:Lucknow,
+        state:"Uttar Pradesh",
+        capital:"Lucknow"
+    },
+    {
+        photo:Kolkata,
+        state:"West Bengal",
+        capital:"Kolkata"
+    } ];
+
+  return (
+    <div >  
+        <h2 className="heading1">States and Capitals</h2>
+        <br/>
+    <Row>   
+     {
+        Data.map((variant, idx) => (
+        <Col md="3" key={idx} className="media-col text-center"> 
+        <Card className="mb-3 card-style" style={{width:'250',height:'250'}}>             
+            <Card.Body style={{padding:'1rem'}}>
+            <Card.Title>                   
+               {variant.state}
+            </Card.Title>
+            <Image src={variant.photo} alt={variant.val} rounded style={{padding:'1rem',width:'200px'}} />                     
+                <br/>
+                {variant.capital}
+            </Card.Body>
+        </Card> 
+        </Col>
+        ))
+     }
+    </Row> 
+    </div>
+  );
+}
+
+export default States;
---
+++ src/Data/union.js
@@ -0,0 +1,19 @@
+
+Union Territories
+Andaman and Nicobar Islands
+(Port Blair)
+Chandigarh
+(Chandigarh)
+Dadra and Nagar Haveli and
+Daman & Diu
+(Daman)
+The Government of NCT of Delhi
+(Delhi)
+Jammu & Kashmir
+(Srinagar-S*, Jammu-W*)
+Ladakh
+(Leh)
+Lakshadweep
+(Kavaratti)
+Puducherry
+(Puducherry)
