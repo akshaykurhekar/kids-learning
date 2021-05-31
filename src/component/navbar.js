1eee7aef6922b6689410f390dd0a104e6d9b9e91
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-05-31 19:31:50 +0530

    font updated

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -9,12 +9,12 @@ function Navbarkids() {
     <Navbar.Brand className="heading" href="/kids-learning/#/">KG E-Book</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="/kids-learning/#/alphabets"><img src="https://img.icons8.com/color/26/000000/alpha.png" alt="Alphabets_icon"/> Alphabets </Nav.Link>
-        <Nav.Link href="/kids-learning/#/numbers"><img src="https://img.icons8.com/color/26/000000/1-circle--v3.png" alt="number_icon"/> Numbers </Nav.Link>
-        <Nav.Link href="/kids-learning/#/words"><img src="https://img.icons8.com/color/26/000000/document--v1.png" alt="words_icon"/> Words </Nav.Link>
-        <Nav.Link href="/kids-learning/#/table"><img src="https://img.icons8.com/color/26/000000/sigma.png" alt="table_icon"/> Table's </Nav.Link>
-        <Nav.Link href="/kids-learning/#/about"><img src="https://img.icons8.com/color/26/000000/about.png" alt="About_icon"/> About Us</Nav.Link>
+      <Nav className="mr-auto pl-5 ">
+        <Nav.Link href="/kids-learning/#/alphabets" className="nav-item"><img src="https://img.icons8.com/color/26/000000/alpha.png" alt="Alphabets_icon"/>Alphabets  </Nav.Link>
+        <Nav.Link href="/kids-learning/#/numbers" className="nav-item" ><img src="https://img.icons8.com/color/26/000000/1-circle--v3.png" alt="number_icon"/> Numbers </Nav.Link>
+        <Nav.Link href="/kids-learning/#/words" className="nav-item"><img src="https://img.icons8.com/color/26/000000/document--v1.png" alt="words_icon"/> Words </Nav.Link>
+        <Nav.Link href="/kids-learning/#/table" className="nav-item"><img src="https://img.icons8.com/color/26/000000/sigma.png" alt="table_icon"/> Table's </Nav.Link>
+        <Nav.Link href="/kids-learning/#/about" className="nav-item"><img src="https://img.icons8.com/color/26/000000/about.png" alt="About_icon"/> About Us</Nav.Link>
       </Nav>
       <Form inline>
           <Button href="/kids-learning/#/practice" variant="outline-success"> Practice <img src="https://img.icons8.com/color/26/000000/edit--v1.png" alt="Practice_icon"/></Button>
